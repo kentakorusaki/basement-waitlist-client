5234142317e6e0e51408e4ec39f06432fcf77ba4
Justin T <sakuwarp91@yahoo.com>
2022-03-22 10:13:17 -0500

    added pacman
    pacman button to add students

--- src/Pages/Waitlist.js
+++ src/Pages/Waitlist.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import axios from "axios";
+import { useEffect } from 'react';
 import Sidebar from '../Components/Sidebar';
 import Topbar from '../Components/Topbar';
+import Wait from '../Components/pictures/pac-man.png';
 
 function Waitlist() {
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts")
+    }, [])
     return (
         <div className="App">
             <Sidebar />
             <Topbar />
             <div className="PageLayout">
-                waiting...
+                <div className="pacman">
+                    <img src={Wait} height = {52} width = {110}/>
+                    <div className="center">Enter a Student</div>
+                </div>
             </div>
         </div>
     )
